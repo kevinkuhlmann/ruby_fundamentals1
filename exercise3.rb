1607fb061e48606cd03694a4241346b529facdf2
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-06-09 15:08:24 -0400

    Acutally complete exercise 3 with age question

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name.capitalize}!"
+
+puts "How old are you?"
+age = gets
+current_year = 2015
+puts "You were born in #{current_year - age.to_i}"
